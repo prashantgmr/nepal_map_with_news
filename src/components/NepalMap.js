6f169a898b6685a2d0d1fea032f3c0241d86da45
prashantgmr <prashantgmr7@gmail.com>
2020-10-12 19:44:20 +0545

    first commit

---
+++ src/components/NepalMap.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect, useRef} from 'react';
+import { Map, GeoJSON} from "react-leaflet";
+import mapData from "./../data/districts.json";
+import "leaflet/dist/leaflet.css";
+import "./Map.css";
+const newsDetail =[
+    {
+        id : 1,
+        news: [ {title: "asdasd", content:"sdfdfsdfsdfsdf"}]
+    },
+    {
+        id: 2,
+        news: [{ title: "asdasd", content: "sdfdfsdfsdfsdf" }]
+    }
+]
+export default function NepalMap() {
+    const [selectDistrict, setSelectDistrict] = useState('');
+    const [news, setNews] = useState([]);
+    const geojson = useRef();
+
+    useEffect(() => {
+        const newsData = newsDetail.filter(item => item.id == selectDistrict);
+        if(newsData.length){
+        setNews(newsData[0].news)
+        }
+        return () => {
+            setNews([])
+        }
+    }, [selectDistrict]);
+
+    var i = 1 ,fillColour;
+    Object.keys(mapData.features).map(
+    (e) => {
+        mapData.features[e]["idx"] = i;
+        i++
+    });
+    const districtStyle = {
+        fillColor: fillColour,
+        fillOpacity: 1,
+        color: "black",
+        weight: .75,
+        };
+    
+    const districtClicked = (e) => {
+        e.target.setStyle({
+            fillColor : "yellow",
+            fillOpacity: 1,
+            weight:1
+        });
+        setSelectDistrict(e.target.feature.idx);
+        
+    }
+   
+    
+    const onEachDistrict = (district, layer) => {
+        const id = district.idx;
+        if (id < 15) {
+            fillColour = "#ffcebc"
+        } else if (id < 23) {
+            fillColour = "#addeff"
+        } else if (id < 36) {
+            fillColour = "#daf7a2"
+        } else if (id < 47) {
+            fillColour = "#e0bdee"
+        } else if (id < 59) {
+            fillColour = "#ffcc78"
+        } else if (id < 69) {
+            fillColour = "#b2ee97"
+        } else {
+            fillColour = "#f5b4ef"
+        }
+
+        layer.options.fillColor = fillColour;
+        const name = district.properties.जिल्ला;
+        layer.bindTooltip(name);
+        layer.bindPopup(name);
+        layer.on({
+            click: districtClicked,
+        });
+        layer.on({
+            mouseout: function (e) {
+                // e.target.setStyle({
+                //     fillColor : fillColour
+                // });
+            }
+        });
+      };
+    return (
+        <div>
+        <Map style={{ height: "80vh" }} center={[28.5,84]} zoom={7.35}>
+    
+          <GeoJSON
+            ref= {geojson}
+            style={districtStyle}
+            data={mapData.features}
+            onEachFeature={onEachDistrict}
+          />
+        </Map>
+
+      </div>
+    )
+}
